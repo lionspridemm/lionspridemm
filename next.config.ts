e379efe6a97fb94deb1264e8b0cc6daaff0fd796
lionspridemm <lionspridemm@gmail.com>
2026-01-13 20:23:40 -0600

    more commit and build issue fixed

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Remove "experimental.appDir" — no longer needed in modern Next.js
   reactStrictMode: true,
-  swcMinify: true,
 };
 
 export default nextConfig;
